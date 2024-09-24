d7add056c5b58e413ca4d21cc3bc5ccfbd0b4e10
katsunori waragai <waragai.katsunori@borgroid.co.jp>
2024-09-24 14:24:59 +0900

    output to file

--- versions.py
+++ versions.py
@@ -5,6 +5,8 @@ def get_versions_in_pypi():
     https://pypi.org/rss/project/opencv-python/releases.xml
     :return:
     """
+    def remove_marker(line):
+        return line.replace("<link>", "").replace("</link>", "").strip()
 
     url = "https://pypi.org/rss/project/opencv-python/releases.xml"
 
@@ -13,11 +15,12 @@ def get_versions_in_pypi():
         body = res.read()
 
     lines = body.decode().splitlines()
-    links = []
-    for line in lines:
-        if line.find("<link>") > -1 and line.find("</link>") > -1:
-            value = line.replace("<link>", "").replace("</link>", "").strip()
-            links.append(value)
+    links = [remove_marker(line) for line in lines if line.find("<link>") > -1 and line.find("</link>") > -1]
     links = sorted(links)
     pprint(links)
+    oname = "versions.txt"
+    with open(oname, "wt") as f:
+        for a in links:
+            f.write(f"{a}\n")
+
 get_versions_in_pypi()
